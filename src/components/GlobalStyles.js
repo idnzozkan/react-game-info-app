fb02feacbb36d66bc331cd719312933ec9f0ebf1
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-04-23 15:16:15 +0300

    Add global styles

---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,60 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        &::-webkit-scrollbar {
+            width: 0.5rem;
+        }
+
+        &::-webkit-scrollbar-thumb {
+            background-color: #F83554;
+        }
+
+        &::-webkit-scrollbar-track {
+            background-color: #232327;
+        }
+
+    }
+
+    body {
+        background: rgb(26,26,29);
+        background: linear-gradient(90deg, rgba(26,26,29,1) 0%, rgba(48,48,54,1) 100%);
+        font-family: 'Montserrat', sans-serif;
+        width: 100%;
+    }
+
+    h2 {
+        font-size: 3rem;
+        font-family: 'Anton', sans-serif;
+        font-weight: lighter;
+        letter-spacing: 0.1rem;
+        word-spacing: 0.5rem;
+        color: #F83554;
+    }
+
+    h3 {
+        font-size: 1.3rem;
+        color: #c56978;
+        padding-top: 1.5rem;
+    }
+
+    p {
+        font-size: 1.2rem;
+        line-height: 200%;
+        color: #999fbb;
+        padding-bottom: 1.5rem;
+    }
+
+    a {
+        text-decoration: none;
+        color: #3D63FF;
+    }
+`;
+
+export default GlobalStyles;

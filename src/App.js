6065b7026bc597f0e68c387e2f7fbf157d4e9fc0
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-04-23 15:15:58 +0300

    Import global styles to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 // Components and pages
+import GlobalStyles from "./components/GlobalStyles";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
+      <GlobalStyles />
       <Home />
     </div>
   );

4cae88aa764106e225c5c1a68c5380e75cbffe8c
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-04-25 00:49:22 +0300

    Create API urls for game detail and screenshots

--- src/api.js
+++ src/api.js
@@ -44,3 +44,7 @@ const new_games = `games?key=${api_key}&dates=${lastYear},${currentDate}&orderin
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${new_games}`;
+export const gameDetailsURL = (game_id) =>
+  `${base_url}games/${game_id}?key=${api_key}`;
+export const screenshotsURL = (game_id) =>
+  `${base_url}games/${game_id}/screenshots?key=${api_key}`;

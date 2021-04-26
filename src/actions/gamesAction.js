d381a615b9bca8c27a9b4e44210df02a29b6755c
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-04-26 17:54:14 +0300

    Add action for game searching

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { newGamesURL, popularGamesURL, upcomingGamesURL } from "../api";
+import {
+  newGamesURL,
+  popularGamesURL,
+  upcomingGamesURL,
+  searchGameURL
+} from "../api";
 
 export const loadGames = () => async (dispatch) => {
   const popularData = await axios.get(popularGamesURL());
@@ -12,6 +17,17 @@ export const loadGames = () => async (dispatch) => {
       popular: popularData.data.results,
       upcoming: upcomingData.data.results,
       newGames: newGamesData.data.results
+    }
+  });
+};
+
+export const searchGame = (game_name) => async (dispatch) => {
+  const searchData = await axios.get(searchGameURL(game_name));
+
+  dispatch({
+    type: "FETCH_SEARCHED_GAME",
+    payload: {
+      searched: searchData.data.results
     }
   });
 };

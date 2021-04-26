307f63c920d8fc18c1b3d5496595a3c4d40b2760
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-04-26 17:57:36 +0300

    Add animations for shared layouts and fetch searched game data to the home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
 // Style and animation
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
 // Components
 import Game from "../components/Game";
 import GameDetail from "../components/GameDetail";
+import { fadeIn } from "../animations";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -17,52 +18,76 @@ const Home = () => {
     dispatch(loadGames());
   }, [dispatch]);
 
-  const { popular, newGames, upcoming } = useSelector((state) => state.games);
+  const { popular, newGames, upcoming, searched } = useSelector(
+    (state) => state.games
+  );
 
   const { pathname } = useLocation();
   const pathId = pathname.split("/")[2];
 
   return (
-    <GameList>
-      {pathId && <GameDetail />}
-      <h2>Upcoming games</h2>
-      <Games>
-        {upcoming.map((game) => (
-          <Game
-            name={game.name}
-            released={game.released}
-            image={game.background_image}
-            id={game.id}
-            key={game.id}
-          />
-        ))}
-      </Games>
+    <GameList variants={fadeIn} initial="hidden" animate="show">
+      <AnimateSharedLayout type="crossfade">
+        <AnimatePresence>{pathId && <GameDetail />}</AnimatePresence>
 
-      <h2>Popular games</h2>
-      <Games>
-        {popular.map((game) => (
-          <Game
-            name={game.name}
-            released={game.released}
-            image={game.background_image}
-            id={game.id}
-            key={game.id}
-          />
-        ))}
-      </Games>
+        {searched.length ? (
+          <div>
+            <h2>Search results</h2>
+            <Games>
+              {searched.map((game) => (
+                <Game
+                  name={game.name}
+                  released={game.released}
+                  image={game.background_image}
+                  id={game.id}
+                  key={game.id}
+                />
+              ))}
+            </Games>
+          </div>
+        ) : (
+          ""
+        )}
 
-      <h2>New games</h2>
-      <Games>
-        {newGames.map((game) => (
-          <Game
-            name={game.name}
-            released={game.released}
-            image={game.background_image}
-            id={game.id}
-            key={game.id}
-          />
-        ))}
-      </Games>
+        <h2>Upcoming games</h2>
+        <Games>
+          {upcoming.map((game) => (
+            <Game
+              name={game.name}
+              released={game.released}
+              image={game.background_image}
+              id={game.id}
+              key={game.id}
+            />
+          ))}
+        </Games>
+
+        <h2>Popular games</h2>
+        <Games>
+          {popular.map((game) => (
+            <Game
+              name={game.name}
+              released={game.released}
+              image={game.background_image}
+              id={game.id}
+              key={game.id}
+            />
+          ))}
+        </Games>
+
+        <h2>New games</h2>
+        <Games>
+          {newGames.map((game) => (
+            <Game
+              name={game.name}
+              released={game.released}
+              image={game.background_image}
+              id={game.id}
+              key={game.id}
+            />
+          ))}
+        </Games>
+      </AnimateSharedLayout>
     </GameList>
   );
 };
